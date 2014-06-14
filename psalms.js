fc77c6be2eda76d3032b5598b25bb50da9c45178
jwwishart <jwwishart>
2014-06-14 23:40:26 +1000

    jquery.mobile removal work
    - Removed jquery.mobile
    - got the first page working
    - got navigation working from psalm selector
    - got back navigation working
    - got psalm text rendering correctly

---
+++ psalms.js
@@ -0,0 +1,78 @@
+
+
+(function($) {
+	var _transitioning = false;
+
+	function hidePage(page, callback) {
+		if (_transitioning === true) {
+			setTimeout(function() {
+				hidePage(page, callback)
+			}, 4);
+
+			return;
+		}
+
+		_transitioning = true;
+
+		_selectElement(page).fadeOut(cs.hidePage.speed, function() {
+			_transitioning = false;
+			
+			if (callback) callback();
+		});
+	}
+
+	function showPage(page, callback) {
+		if (_transitioning === true) {
+			setTimeout(function() {
+				showPage(page, callback)
+			}, 4);
+			
+			return;
+		}
+
+		_transitioning = true;
+
+		// Hide pages that are showing
+		$(".page:visible").hide();
+
+		// Show the page
+		_selectElement(page).fadeIn(cs.showPage.speed, function() {
+			_transitioning = false;
+
+			if (callback) callback();
+		});
+	}
+
+	function _selectElement(idElementOrjQuery) {
+		var hash = '#';
+
+		// String
+		if ($.type(idElementOrjQuery) === 'string') {
+			idElementOrjQuery = $.trim(idElementOrjQuery);
+
+			// If has a hashe don't add one to query...
+			if (idElementOrjQuery.indexOf(hash) != -1) {
+				hash = ''; 
+			}
+
+			idElementOrjQuery = $(hash + idElementOrjQuery);
+		}
+
+		// Element assumed
+		if (!(idElementOrjQuery instanceof jQuery)) {
+			idElementOrjQuery = $(idElementOrjQuery); // assumed to be an element
+		}
+
+		return idElementOrjQuery;
+	}
+
+	// Export
+	//
+
+	window.cs.hidePage = hidePage;
+	window.cs.hidePage.speed = 700;
+
+	window.cs.showPage = showPage;
+	window.cs.showPage.speed = 700;
+
+}(jQuery));
